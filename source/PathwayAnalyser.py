cf480ec9d050c7dbe6c9a1e93821910a3455e7ed
dinithins <dinithins1990@gmail.com>
2022-11-01 20:37:10 +0000

    added overrepresentation analysis using g:Profiler

--- source/PathwayAnalyser.py
+++ source/PathwayAnalyser.py
@@ -1,6 +1,11 @@
 from gsea_api.molecular_signatures_db import MolecularSignaturesDatabase
+import scanpy as sc
+from gprofiler import GProfiler
+from tabulate import tabulate
+from tqdm import tqdm
 import enum
 import numpy as np
+import pandas as pd
 
 class DB_NAMES(enum.Enum):
     
@@ -45,6 +50,78 @@ class PathwayAlignmentObj:
     def __init__(self, aligner):
         self.aligner = aligner
         
+        
+        
+class PathwayEnrichmentAnalyser:
+    
+    def __init__(self, aligner):
+        self.aligner = aligner
+        
+    def make_ranked_genelist(self):
+        if hasattr(self, 'x'):
+            return
+        #print('make ranked gene list')
+        matched_percentages = {}
+        for al_obj in self.aligner.results:
+            matched_percentages[al_obj.gene] = (al_obj.get_series_match_percentage()[0]/100 )
+        x = sorted(matched_percentages.items(), key=lambda x: x[1], reverse=False)
+        self.x = x 
+        return x
+        
+    # runs gprofiler overrepresentation analysis over the top k DE genes (k decided on matched percentage threshold) 
+    def run_gprofiler(self, DIFF_THRESHOLD=0.5):
+        self.make_ranked_genelist()
+        top_k = np.unique(pd.DataFrame(self.x)[1] < DIFF_THRESHOLD , return_counts=True)[1][1]
+        print(top_k, ' # of DE genes to check')
+        clusters = pd.DataFrame([pd.DataFrame(self.x)[0:top_k][0], np.repeat(0,top_k)]).transpose()
+        clusters.columns = ['Gene','ClusterID']
+        clusters = clusters.set_index('Gene')
+        self.gprofiler_results = sc.queries.enrich(list(clusters.index))
+        return self.gprofiler_results
+    
+    # runs gprofiler overrepresentationa analysis for each cluster 
+    def run_gprofiler_for_alignment_clusters(self):
+        
+        self.make_ranked_genelist()
+        #list(clusters.index)
+        gprofiler_results_all = []
+        for cluster_id in tqdm(range(len(self.aligner.gene_clusters))):
+            #print('processing cluster: ', cluster_id)
+            temp = pd.DataFrame(self.x)
+            temp = temp.set_index(0)
+            temp = temp.filter(items = self.aligner.gene_clusters[cluster_id], axis=0)
+            clusters = temp
+            clusters['Gene'] = clusters.index
+            clusters.columns = ['ClusterID','Gene']
+            clusters['ClusterID'] = np.repeat(cluster_id, len(clusters))
+            #print('# of genes: ', len(clusters))
+            #print(list(clusters.index))
+            gprofiler_results = sc.queries.enrich(list(clusters.index))
+            gprofiler_results_all.append(gprofiler_results)
+            
+        return gprofiler_results_all
+    
+    # KEGG and REAC focused 
+    def run_cluster_enrichment(self):
+    
+        gprofiler_results_all = self.run_gprofiler_for_alignment_clusters()
+        cluster_overrepresentation_results = []
+        for cluster_id in range(len(self.aligner.gene_clusters)):
+            _kegg=gprofiler_results_all[cluster_id][gprofiler_results_all[cluster_id].source == 'KEGG']
+            _reac=gprofiler_results_all[cluster_id][gprofiler_results_all[cluster_id].source == 'REAC']
+            if(len(_kegg)>0 or len(_reac)>0):
+               # print('Cluster:', cluster_id, ' ----- ', len(self,aligner.gene_clusters[cluster_id])) 
+                cluster_overrepresentation_results.append([cluster_id,len(self.aligner.gene_clusters[cluster_id]), 
+                                                           self.aligner.gene_clusters[cluster_id],np.asarray(_kegg.name) ,np.asarray(_reac.name) ])
+        self.results = pd.DataFrame(cluster_overrepresentation_results)
+        print(tabulate(self.results,  headers=['cluster_id','n_genes', 'geneset', 'KEGG_pathways','REACTOME_pathways'],tablefmt="grid",maxcolwidths=[3, 3, 3,30,50,50])) 
+        
+    def write_results_csv(self, file_name, write_markdown=False):
+            self.results.to_csv(file_name + '.csv')
+            if(write_markdown):
+                self.results.to_markdown(file_name + '.md')
+        
+    
 # GOLDRATH_NAIVE_VS_EFF_CD8_TCELL_DN
 # GOBP_INTERFERON_ALPHA_PRODUCTION
 
